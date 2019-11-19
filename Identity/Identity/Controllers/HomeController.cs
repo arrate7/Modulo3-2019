3f17a48f84b34f75519db2f02066c07bebfba98f
arrate ortiz <arrateoc@gmail.com>
2019-11-19 13:17:06 +0100

    .

---
+++ Identity/Identity/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger,
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Administrador()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                IdentityUser user = await _userManager.GetUserAsync(User);
+
+                if (await _userManager.IsInRoleAsync(user, "Administrador"))
+                {
+                    return View();
+                }
+            }
+            return NotFound() ;
+        }
+
+        public IActionResult Privacy()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return View();
+            }
+            return NotFound();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
